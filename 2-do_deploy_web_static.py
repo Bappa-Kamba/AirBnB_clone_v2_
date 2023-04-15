aa2c17de75e9522d394661769f89dc4b1d57efca
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-04-15 15:23:15 +0100

    use sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,30 +7,59 @@ Usage:
     do_deploy:archive_path=versions/web_static_20170315003959.tgz
     -i my_ssh_private_key -u ubuntu
 """
-from fabric.api import env, put, run
+from fabric.api import env, put, sudo
 import os.path
+
 env.hosts = ['54.197.82.225', '54.157.180.23']
+env.user = 'ubuntu'
+env.key_filename = os.path.expanduser('~/.ssh/school')
 
 
 def do_deploy(archive_path):
     """
-    Deploy archive to web server
+    Distribute an archive to your web servers.
+
+    Args:
+        archive_path: The path to the archive to deploy.
+
+    Returns:
+        True if all operations have been done correctly, otherwise returns False.
     """
-    if os.path.isfile(archive_path) is False:
+    # Check if the archive exists
+    if not os.path.isfile(archive_path):
         return False
+
+    # Get the archive filename (without extension)
+    filename = os.path.splitext(os.path.basename(archive_path))[0]
+
     try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # Create the releases directory if it doesn't exist
+        sudo('mkdir -p /data/web_static/releases/{}/'.format(filename))
+
+        # Uncompress the archive to the releases directory
+        sudo('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(filename, filename))
+
+        # Delete the archive from the web server
+        sudo('rm /tmp/{}.tgz'.format(filename))
+
+        # Move the contents of the release directory up one level
+        sudo('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(filename, filename))
+
+        # Remove the now-empty web_static directory
+        sudo('rmdir /data/web_static/releases/{}/web_static'.format(filename))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        sudo('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link /data/web_static/current
+        sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename))
+
+        # Deployment successful
+        print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
